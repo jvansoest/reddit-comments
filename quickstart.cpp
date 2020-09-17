66adb585c781deae29bd1949003328cb2b24d7b7
jevsoest <jeroen.van.soest@vub.be>
2020-09-17 22:22:30 +0200

    added word frequency

--- quickstart.cpp
+++ quickstart.cpp
@@ -1,63 +1,64 @@
 #include "simdjson.h"
-
-std::vector<std::string_view> SplitSV(const std::string_view str, const char delim = ' ')
-{   
-    std::vector<std::string_view> result;
-
-    int indexCommaToLeftOfColumn = 0;
-    int indexCommaToRightOfColumn = -1;
-
-    for (int i=0;i<static_cast<int>(str.size());i++)
-    {
-        if (str[i] == delim)
-        {
-            indexCommaToLeftOfColumn = indexCommaToRightOfColumn;
-            indexCommaToRightOfColumn = i;
-            int index = indexCommaToLeftOfColumn + 1;
-            int length = indexCommaToRightOfColumn - index;
-
-            // Bounds checking can be omitted as logically, this code can never be invoked 
-            // Try it: put a breakpoint here and run the unit tests.
-            /*if (index + length >= static_cast<int>(str.size()))
-            {
-                length--;
-            }               
-            if (length < 0)
-            {
-                length = 0;
-            }*/
-
-            std::string_view column(str.data() + index, length);
-            result.push_back(column);
-        }
-    }
-    const std::string_view finalColumn(str.data() + indexCommaToRightOfColumn + 1, str.size() - indexCommaToRightOfColumn - 1);
-    result.push_back(finalColumn);
-    return result;
-}
-
-
-
+#include <map>
 
 int main(void) {
-  simdjson::dom::parser parser;
-  simdjson::dom::element tweets = parser.load("twitter.json");
-  std::cout << tweets["search_metadata"]["count"] << " results." << std::endl;
+  //simdjson::dom::parser parser;
+  //simdjson::dom::element tweets = parser.load("twitter.json");
+  //std::cout << tweets["search_metadata"]["count"] << " results." << std::endl;
   
   simdjson::dom::parser parser2;
   simdjson::dom::document_stream docs = parser2.load_many("RC_2006-12");
-  for (simdjson::dom::element doc : docs) {
-      std::cout << doc["created_utc"] << std::endl;
-      std::string_view body  = doc["body"].get_string();
-      std::cout << body << std::endl;
-      std::vector<std::string_view> tokens = SplitSV(body, ' ');	
-      
-      for (int i = 0; i<tokens.size(); i++){
-         std::cout << body.at(i) << ' ';
-      }
 
+
+  std::map<std::string, int> counts;
+
+  for (simdjson::dom::element doc : docs) {
+      //std::cout << doc["created_utc"] << std::endl;
+      std::string_view str  = doc["body"].get_string();
+      //std::cout << body << std::endl;
+            
+      std::string word = "";
+
+      for (int i = 0; i<str.size(); i++){
+
+         // check if current char is blank space then it means we got one word
+	if (str[i] == ' '){
+
+	  // if the current word is not found then insert current word with freq 1
+          if (counts.find(word) == counts.end()){
+	    counts.insert(std::make_pair(word, 1));
+	    word = "";
+	  }
+
+	  //update the frequency
+	  else {
+	    counts[word]++;
+	    word = "";
+	  }
+        } else {
+	 word += str[i];	
+        }
+     }
+     // storing the last word of the string
+     if (counts.find(word) == counts.end()){
+       counts.insert(std::make_pair(word, 1));
+     } else {
+       counts[word]++;	
+     }
+
+     // traverse the map to print the frequency
+     /*for (auto& it: counts){
+       std::cout << it.first << " - " << it.second << std::endl;
+     }*/
 
   }
+
+
+
+
+
+
+
 
 
 }
